f17a5f045e936d5dcf463f57eff5941fa8316ca2
JakubMierzynski <134516571+JakubMierzynski@users.noreply.github.com>
2023-12-26 17:10:53 +0100

    Collision with one asteroid works.
    Collision with asteroid will restart. At the moment it's enough.
    Also made some quick fixes on other files to make them a little more... readable

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class Game(object):
 
         # Initialization
         pygame.init()
+        self.pause_text = pygame.font.SysFont('Consolas', 32).render('Pause', True, pygame.color.Color('White'))
         self.player = Rocket(self)
         self.screen = pygame.display.set_mode(size=self.resolution)
         self.tps_clock = pygame.time.Clock()
@@ -38,15 +39,20 @@ class Game(object):
         self.scroll = 0
         self.scroll_vel = 1
 
+        RUNNING, PAUSE = 0, 1
+        state = RUNNING
+
         while True:
             # Handle event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit(0)
-
-            # If player hits border: Restart.
-            if self.player.pos.x <= 0 or self.player.pos.y <= 0 or self.player.pos.x >= 1280 or self.player.pos.y >= 720:
-                Game()
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_p and state is RUNNING:
+                        state = PAUSE
+                        self.pause()
+                    else:
+                        state = RUNNING
 
             # Ticking
             self.tps_delta += self.tps_clock.tick() / 1000.0
@@ -54,29 +60,50 @@ class Game(object):
                 self.tick()
                 self.tps_delta -= 1 / self.max_tps
 
-            # DRAWING
-            self.screen.fill(color="black")
+            if state is RUNNING:
+                # DRAWING
+                self.screen.fill(color="black")
 
-            # Drawing Background
-            for i in range(0, self.tiles):
-                self.screen.blit(self.background, (i * self.background_width + self.scroll, 0))
-            # Changing scroll to make movement
-            self.scroll -= self.scroll_vel
+                # Drawing Background
+                for i in range(0, self.tiles):
+                    self.screen.blit(self.background, (i * self.background_width + self.scroll, 0))
+                # Changing scroll to make movement
+                self.scroll -= self.scroll_vel
 
-            # Reset scroll to make it infitnie
-            if abs(self.scroll) > self.background_width:
-                self.scroll = 0
+                # Reset scroll to make it infitnie
+                if abs(self.scroll) > self.background_width:
+                    self.scroll = 0
 
-            # Draw border of the game
-            pygame.draw.rect(self.screen, "red", (0, 0, 1279, 719), 3)
+                # Draw border of the game
+                pygame.draw.rect(self.screen, "red", (0, 0, 1279, 719), 3)
 
-            # Use Draw Function that draws player and later obstacles
-            self.draw()
+                # Check masks overlap if collision- restart
+                if self.player.rocket_mask.overlap(
+                        self.asteroid.asteroid_mask, (self.asteroid.pos[0] - self.player.pos[0],
+                                                      self.asteroid.pos[1] - self.player.pos[1])):
+                    Game()
+                else:
+                    pass
+
+                # Use Draw Function that draws player and later obstacles
+                self.draw()
+
+            if state is PAUSE:
+                self.pause()
+
     def tick(self):
         self.player.tick()
         self.asteroid.tick()
 
+    def pause(self):
+        pause_text = pygame.font.SysFont('Consolas', 32).render('Pause', True, pygame.color.Color('White'))
+        self.screen.blit(pause_text, (0, 0))
+
     def draw(self):
+        # If player hits border: Restart.
+        if self.player.pos.x <= 0 or self.player.pos.y <= 0 or self.player.pos.x >= 1280 or self.player.pos.y >= 720:
+            Game()
+
         # Drawing Background
         for i in range(0, self.tiles):
             self.screen.blit(self.background, (i * self.background_width + self.scroll, 0))
